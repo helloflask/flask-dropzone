929ad037bc8a6696e53cbe06bb4cd4350315ea1f
Grey Li <withlihui@gmail.com>
2018-06-30 17:37:31 +0800

    Add in-form dropzone example application

---
+++ examples/in-form/app.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import os
+
+from flask import Flask, render_template, request, redirect
+from flask_dropzone import Dropzone
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config.update(
+    UPLOADED_PATH=os.path.join(basedir, 'uploads'),
+    # Flask-Dropzone config:
+    DROPZONE_ALLOWED_FILE_TYPE='image',
+    DROPZONE_MAX_FILE_SIZE=3,
+    DROPZONE_MAX_FILES=30,
+    DROPZONE_UPLOAD_IN_FORM=True,
+    DROPZONE_UPLOAD_ON_CLICK=True,
+    DROPZONE_UPLOAD_ACTION='handle_upload',  # URL or endpoint
+    DROPZONE_UPLOAD_BTN_ID='submit',
+)
+
+dropzone = Dropzone(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def handle_upload():
+    for key, f in request.files.iteritems():
+        if key.startswith('file'):
+            f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+    return '', 204
+
+
+@app.route('/form', methods=['POST'])
+def handle_form():
+    title = request.form.get('title')
+    description = request.form.get('description')
+    return 'file uploaded and form submit<br>title: %s<br> description: %s' % (title, description)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
