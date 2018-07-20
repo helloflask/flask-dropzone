7175b71bab046abfc39ceadab98376bba4a8c072
Grey Li <withlihui@gmail.com>
2018-07-20 10:47:38 +0800

    Add example for large file upload

---
+++ examples/large-file/app.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import os
+
+from flask import Flask, render_template, request
+from flask_dropzone import Dropzone
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config.update(
+    UPLOADED_PATH=os.path.join(basedir, 'uploads'),
+    # Flask-Dropzone config:
+    DROPZONE_MAX_FILE_SIZE=1024,  # set max size limit to a large number, here is 1024 MB
+    DROPZONE_TIMEOUT=5 * 60 * 1000  # set upload timeout to a large number, here is 5 minutes
+)
+
+dropzone = Dropzone(app)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        f = request.files.get('file')
+        f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
