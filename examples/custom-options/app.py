49ddd21653c40d463fff16e4ffd405fb02a2a08c
Grey Li <withlihui@gmail.com>
2018-07-20 11:31:06 +0800

    Add example appliction for custom options

---
+++ examples/custom-options/app.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import os
+
+from flask import Flask, render_template, request
+from flask_dropzone import Dropzone
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config.update(
+    UPLOADED_PATH=os.path.join(basedir, 'uploads'),
+    # Flask-Dropzone config:
+    DROPZONE_ALLOWED_FILE_TYPE='image',
+    DROPZONE_MAX_FILE_SIZE=3,
+    DROPZONE_MAX_FILES=30,
+)
+
+dropzone = Dropzone(app)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        f = request.files.get('file')
+        f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
